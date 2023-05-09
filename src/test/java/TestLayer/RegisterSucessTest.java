daec1ee2605d9bb50d6bb61a27e3480a3d2c6f27
unknown <sonali.bhosale2725@gmail.com>
2023-05-09 20:28:15 +0530

    framework code added

---
+++ src/test/java/TestLayer/RegisterSucessTest.java
@@ -0,0 +1,68 @@
+package TestLayer;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.BaseLayer.BaseClass;
+
+import PageLayer.RegisterSucess;
+
+@Test(groups = { "RegisterSucess" }, dependsOnGroups = { "RegisterPage" })
+public class RegisterSucessTest extends BaseClass {
+	private RegisterSucess registersucess;
+
+	@Test(priority = 1)
+	public void validateSucessPageUrl() {
+		registersucess = new RegisterSucess();
+		Assert.assertEquals(true, registersucess.captureSucessPageUrl().contains("sucess"));
+	}
+
+	@Test(priority = 2)
+	public void validateFirstName() {
+		Assert.assertEquals(true, registersucess.captureFirstAndLastname().contains(prop.getProperty("fname")));
+	}
+
+	@Test(priority = 3)
+	public void validateLastName() {
+		Assert.assertEquals(true, registersucess.captureFirstAndLastname().contains(prop.getProperty("lname")));
+	}
+
+	@Test(priority = 4)
+	public void validateEmailId() {
+		Assert.assertEquals(true, registersucess.captureUsername().contains(prop.getProperty("uname")));
+	}
+	
+	
+	@Test
+	public void abc()
+	{
+		Assert.assertEquals(true, true);
+	}
+	
+	
+
+	@Test
+	public void xyz()
+	{
+		Assert.assertEquals(true, true);
+	}
+	
+
+	@Test
+	public void pqr()
+	{
+		Assert.assertEquals(true, true);
+	}
+	
+	@Test
+	public void pqr1()
+	{
+		Assert.assertEquals(true, true);
+	}
+	
+	
+	
+	
+	
+
+}
